c0bfbf622bba30d25e6cd4b67cb564bc6c2e69cd
Andrei Kazelski <andkaz@protonmail.com>
2021-12-02 21:14:43 +0100

    used Local Storage, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Grid, Paper} from "@mui/material";
 import {Counter} from "./components/Counter";
 
 function App() {
     let [count, setCount] = useState(0)
+
+    useEffect(() => {
+        let countFromLocalStorage = localStorage.getItem('counter_value')
+        if (countFromLocalStorage) {
+            setCount(JSON.parse(countFromLocalStorage))
+        }
+    }, [])
+
+
+    useEffect(() => {
+        localStorage.setItem('counter_value', JSON.stringify(count))
+    }, [count])
+
 
     const incrementCounter = () => {
         setCount(count + 1)
